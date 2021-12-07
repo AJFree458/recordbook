47d9164288b5a774e76da099019f9ca1a9dd2812
AJFree458 <rthr.frmn@gmail.com>
2021-12-07 17:28:05 -0500

    Change port and general setup

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 3001;
+// dotenv setup
+require("dotenv").config();
+
+// express async errors module
+require("express-async-errors");
+
+// express
+const express = require("express");
 const app = express();
 
+// rest of packages
+const path = require("path");
+const morgan = require("morgan");
+
+// database connection
+
+// routers
+
+// middleware, error middleware
+
+// routes
+app.use(morgan("tiny"));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
+// define the base api route
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
